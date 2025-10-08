b9bb2ee4cc36b47fa30dd72a729fe7ea767fea97
Tang Tao <2024111155@stu.sufe.edu.cn>
2025-10-08 15:22:35 +0800

    Format the code and improve a little bit

--- c file/linkedlist_byhand.c
+++ c file/linkedlist_byhand.c
@@ -1,42 +1,40 @@
-#include<stdio.h>
-#include<stdlib.h>
-typedef struct node
-{
-    int data;
-    struct node *next;
-}Node;
-void printlist(Node *first);
-int main()
-{
+#include <stdio.h>
+#include <stdlib.h>
 
-    Node *first=NULL;
-    //Node *last=first;
-    int n=0;
-    printf("The number of the nodes:");
-    scanf("%d",&n);
-    for(int i=1;i<=n;i++)
-    {
-        Node *newnode=(Node *)malloc(sizeof(Node));
-        newnode->next=NULL;
-        printf("Node %d:",i);
-        scanf("%d",&newnode->data);
-        if(first==NULL)
-        {
-            first=newnode;
-        }
-        else
-        {
-            first->next=newnode;
-        }
-        first=newnode;
-    }
-    printlist(first);
+typedef struct node {
+  int data;
+  struct node* next;
+} Node;
+
+void printlist(Node* first);
+
+int main() {
+  Node* first = NULL;
+
+  int n = 0;
+  printf("The number of the nodes:");
+  scanf("%d", &n);
+
+  for (int i = 1; i <= n; i++) {
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    newNode->next = NULL;
+
+    printf("Node %d:", i);
+    scanf("%d", &newNode->data);
+
+    if (first == NULL)
+      first = newNode;
+    else
+      first->next = newNode;
+
+    first = newNode;
+  }
+  printlist(first);
 }
-void printlist(Node *first)
-{
-    while(first !=NULL)
-    {
-        printf("%d ",first->data);
-        first=first->next;
-    }
+
+void printlist(Node* ptr) {
+  while (ptr != NULL) {
+    printf("%d ", ptr->data);
+    ptr = ptr->next;
+  }
 }
--- c file/reverse.c
+++ c file/reverse.c
@@ -1,34 +1,27 @@
-#include<stdio.h>
-#include<string.h>
-int main()
-{
-    char strread[100];
-    gets(strread);
-    char word[10][10];
-    char temstr[100];
-    int wordnum=0,n=0,m=0;
-    for (int i=0;i<=strlen(strread);i++)
-    {
-        if(strread[i]==' ')
-        {
-            strcpy(word[m],temstr);
-            m++;
-            for(int j=0;j<=n;j++)
-            {
-                temstr[j]='\0';
-            }
-            n=0;
-        }
-        else
-        {
-            temstr[n]=strread[i];
-            n++;
-        }
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+  char strread[100];
+  gets(strread);
+
+  char word[10][10];
+  char temstr[100];
+  int wordnum = 0, n = 0, m = 0;
+  for (int i = 0; i <= strlen(strread); i++) {
+    if (strread[i] == ' ') {
+      strcpy(word[m], temstr);
+      m++;
+      for (int j = 0; j <= n; j++)
+        temstr[j] = '\0';
+      n = 0;
+    } else {
+      temstr[n] = strread[i];
+      n++;
     }
-    strcpy(word[m++],temstr);
-    m-=1;
-    for(;m>=0;m--)
-    {
-        printf("%s ",word[m]);
-    }
+  }
+  strcpy(word[m++], temstr);
+  m -= 1;
+  for (; m >= 0; m--)
+    printf("%s ", word[m]);
 }
